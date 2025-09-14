0f0f4a59ccd6fe130782dd1ad86860a65d998cee
pwlgk <pawelgeyko@gmail.com>
2025-09-14 09:47:41 +0600

    add new logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
-import { LogoIcon } from "@/components/icons/logo-icon";
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-center">
         <Link href="/" className="flex items-center justify-center">
-          <LogoIcon className="h-10 w-10 text-foreground rounded-full" />
+          <Image
+            src="/logo.jpg" // Путь к вашему логотипу в папке /public
+            alt="Логотип приложения"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
         </Link>
       </div>
     </header>
