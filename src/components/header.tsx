b0ce63b4e91a59c164348f8b4e586ef1fa7b3dd6
pwlgk <pawelgeyko@gmail.com>
2025-09-14 10:10:24 +0600

    fix header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-9 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center justify-center">
         <Link href="/" className="flex items-center justify-center">
           <Image
